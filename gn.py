842fef4c96d4433781895f0b73c04edfdca3c560
Anjali_Shivani <114407895+Astrogirlanajli@users.noreply.github.com>
2024-06-27 15:22:33 +1000

    Update gn.py
    Test code with reference to LFOA

--- gn.py
+++ gn.py
@@ -1,88 +1,106 @@
-import pytest
-from pytest import approx
 import os
-from os import path as pth
+import pytest
 import numpy as np
-from astropy.io import fits
-import scopesim
-from scopesim import rc
+from astropy.io.fits import HDUList
 from matplotlib import pyplot as plt
 from matplotlib.colors import LogNorm
+import scopesim
 
-import os.path as pth
+from scopesim import rc
+from scopesim.source.source_templates import star_field
+import scopesim_templates as sim_tp
+
+PLOTS = False
 
 if rc.__config__["!SIM.tests.run_integration_tests"] is False:
     pytestmark = pytest.mark.skip("Ignoring DREAMS integration tests")
 
 # Set TOP_PATH to the directory containing the DREAMS package
 TOP_PATH = "/Users/anjali/Desktop"
-
 rc.__config__["!SIM.file.local_packages_path"] = TOP_PATH
 
 # Adjust the PKGS dictionary to reflect the correct path
-PKGS = {"DREAMS": pth.join(TOP_PATH, "DREAMS")}
+PKGS = {"DREAMS": os.path.join(TOP_PATH, "DREAMS")}
 
 # Verify the path to the DREAMS package
-if not pth.exists(PKGS["DREAMS"]):
+if not os.path.exists(PKGS["DREAMS"]):
     raise FileNotFoundError(f"DREAMS package not found at {PKGS['DREAMS']}")
 else:
     print("DREAMS package found at:", PKGS["DREAMS"])
 
-PLOTS = True
+class TestLoads:
+    def test_scopesim_loads_package(self):
+        dreams = scopesim.OpticalTrain("DREAMS")
+        assert isinstance(dreams, scopesim.OpticalTrain)  # Corrected syntax
+        print("scopesim package loaded successfully.")
 
-class TestInit:
-    def test_all_packages_are_available(self):
-        rc_local_path = rc.__config__["!SIM.file.local_packages_path"]
-        for pkg_name in PKGS:
-            assert os.path.isdir(os.path.join(rc_local_path, pkg_name))
-        print("irdb" not in rc_local_path)
+class TestObserves:
+    def test_something_comes_out(self):
+        print("Starting observation test...")
+        src = star_field(10000, 10, 20, width=700)
 
-class TestLoadUserCommands:
-    def test_user_commands_loads_without_throwing_errors(self, capsys):
-        cmd = scopesim.UserCommands(use_instrument="DREAMS")
-        assert isinstance(cmd, scopesim.UserCommands)
+        cmds = scopesim.UserCommands(use_instrument="DREAMS")
+        cmds["!OBS.dit"] = 10
+        cmds["!DET.bin_size"] = 1
+        cmds["!OBS.sky.bg_mag"] = 14.9
+        cmds["!OBS.sky.filter_name"] = "J"
 
-        stdout = capsys.readouterr()
-        assert len(stdout.out) == 0
+        dreams = scopesim.OpticalTrain(cmds)
+        dreams["detector_linearity"].include = False
+        dreams.observe(src)
+        hdus = dreams.readout()
 
-class TestMakeOpticalTrain:
-    def __init__(self):
-        self.hdu_list = None
+        print(f"Observation completed. HDUList type: {type(hdus[0])}")
 
-    def test_load_lfao(self):
-        cmd = scopesim.UserCommands(use_instrument="DREAMS",
-                                    properties={"!OBS.filter_name": "J",
-                                                "!OBS.dit": 10,
-                                                "!DET.bin_size": 1,
-                                                "!OBS.sky.bg_mag": 14.9,
-                                                "!OBS.sky.filter_name": "J"})
-        opt = scopesim.OpticalTrain(cmd)
-        opt["detector_linearity"].include = False
-        assert isinstance(opt, scopesim.OpticalTrain)
-
-        src = scopesim.source.source_templates.star_field(10000, 10, 20, 700)
-        opt.observe(src)
-        self.hdu_list = opt.readout()[0]
-
-        assert isinstance(self.hdu_list, fits.HDUList)
-
-        print(np.average(self.hdu_list[1].data))
         if PLOTS:
-            self.plot_data()
+            plt.subplot(121)
+            wave = np.arange(3000, 11000)
+            plt.plot(wave, dreams.optics_manager.surfaces_table.throughput(wave))
 
-    def plot_data(self):
-        if self.hdu_list is not None:
-            for my_hdu in self.hdu_list[1:]:
-                plt.imshow(self.hdu_list[1].data, norm=LogNorm())
-                plt.colorbar()  # Add a colorbar to the plot
-                plt.title("Observed Star Field")  # Add a title to the plot
-                plt.xlabel("X Pixels")  # Add an x-axis label
-                plt.ylabel("Y Pixels")  # Add a y-axis label
-                plt.show()
+            plt.subplot(122)
+            im = hdus[0][1].data
+            plt.imshow(im, norm=LogNorm())
+            plt.colorbar()
+            plt.title("Observed Star Field")
+            plt.xlabel("X Pixels")
+            plt.ylabel("Y Pixels")
+            plt.show()
+
+    @pytest.mark.slow
+    def test_observes_from_scopesim_templates(self):
+        print("Starting scopesim templates observation test...")
+        src = sim_tp.stellar.cluster(mass=10000, distance=2000, core_radius=1)
+
+        dreams = scopesim.OpticalTrain("DREAMS")
+        dreams.observe(src)
+
+        dreams.cmds["!OBS.dit"] = 10
+        hdus = dreams.readout()
+
+        assert isinstance(hdus[0], HDUList)
+        print("Observation from scopesim templates completed.")
+
+        if PLOTS:
+            im = hdus[0][1].data
+            plt.imshow(im, norm=LogNorm(), cmap="hot")
+            plt.colorbar()
+            plt.show()
+
+    @pytest.mark.slow
+    def test_saves_readout_to_disc(self):
+        print("Starting test to save readout to disk...")
+        src = sim_tp.stellar.cluster(mass=10000, distance=2000, core_radius=1)
+        dreams = scopesim.OpticalTrain("DREAMS")
+        dreams.observe(src)
+        dreams.readout(filename="GNANU.fits")
+
+        assert os.path.exists("GNANU.fits")
+        print("Readout saved to GNANU.fits.")
 
 def run_test_and_plot():
-    test_optical_train = TestMakeOpticalTrain()
-    test_optical_train.test_load_lfao()
+    test_observes = TestObserves()
+    test_observes.test_something_comes_out()
 
 # Run the test and plot as soon as the module is imported
 run_test_and_plot()
+
