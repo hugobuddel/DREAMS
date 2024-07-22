e19d7c47150c09ea16d00602b6b91c3921627fdf
Anjali_Shivani <114407895+Astrogirlanajli@users.noreply.github.com>
2024-07-22 22:48:07 +1000

    Update Galaxy.py
    Error for syntax src= sim_tp.extragalactic.galaxy

--- Galaxy.py
+++ Galaxy.py
@@ -34,8 +34,8 @@ else:
     print("DREAMS package found at:", PKGS["DREAMS"])
 
 cmds = scopesim.UserCommands(use_instrument="DREAMS")
-cmds["!OBS.dit"] = 1000
-cmds["!OBS.ndit"] = 1000
+cmds["!OBS.dit"] = 10
+cmds["!OBS.ndit"] = 10
 cmds["!DET.bin_size"] = 1
 cmds["!OBS.sky.bg_mag"] = 14.9
 cmds["!OBS.sky.filter_name"] = "J"
@@ -53,8 +53,7 @@ dreams.fov_manager._fovs_list = list(dreams.fov_manager.generate_fovs_list())
 
 print("scopesim package loaded successfully.")
 #src = sim_tp.stellar.clusters.cluster(mass=10000,  distance=50000, core_radius=70, seed=9002)
-src= galaxy("kc96/s0", z=0.1, amplitude=17, filter_curve="J", pixel_scale=2.48, r_eff=2.5, n=4, ellip=0.5, theta=45, extend=3)
-
+src= sim_tp.extragalactic.galaxy("kc96/s0", z=2.48, amplitude= 14.5, filter_curve="J", pixel_scale=, r_eff=2.5, n=4, ellip=0.5, theta=45, extend=3)
 dreams.observe(src, update=False)
 print("yessss anjali")
 hdus = dreams.readout()
@@ -64,12 +63,11 @@ wave = np.arange(3000, 11000)
 plt.plot(wave, dreams.optics_manager.surfaces_table.throughput(wave))
 plt.subplot(122)
 im = hdus[0][1].data
-# detector_order = [2, 1, 4, 3, 6, 5]
 detector_order = [1, 2, 3, 4, 5, 6]
 plt.figure(figsize=(20, 20))
 for plot_number, hdu_number in enumerate(detector_order, 1):
     plt.subplot(3, 2, plot_number)
-    plt.imshow(hdus[0][hdu_number].data, norm=LogNorm(), cmap="hot")
+    plt.imshow(np.log10(src.fields[0].data), origin="lower")
     plt.colorbar()
     
 plt.show()
